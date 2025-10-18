fb470b0423cfd86fa8cd035c784d7a72b061d865
Shivam Modi <smodi9846@gmail.com>
2025-10-18 11:29:44 +0530

    Update cookie settings for authentication

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -38,8 +38,8 @@ const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "strict",
-      secure: false, // true in production
+      sameSite: "None",
+      secure: true, // true in production
     });
 
     // Send safe response
@@ -74,8 +74,8 @@ const signin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      sameSite: "strict",
-      secure: false, // true in production
+      sameSite: "None",
+      secure: true, // true in production
     });
 
     // Send safe user info
